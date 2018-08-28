3d6d0b58f01d55e391b1b7ce14fd1cf8c1a28c62
tawfiknasser <tawfiknasser@gmail.com>
2018-08-28 12:30:57 +0300

    #5 fixed the logic

--- src/game.js
+++ src/game.js
@@ -8,7 +8,7 @@ class game {
                 [9,0,0,0,9],
                 [9,0,0,0,9]
                 [9,9,9,9,9]]; /// 999 is the border
-                              /// 111 for X
+                       /// 111 for X
                               /// 222 for O
 
 
@@ -33,36 +33,49 @@ checkFull (counter){
 //   return false ;
 // }
 
+const one=1;
+const two=2;
+const three=3;
+
 checkVertical(x,y){
   //moving the y
-  if(this.board[x][y]==this.board[x][y+1]
-    && this.board[x][y-1]==this.board[x][y] && this.board[x][y]!== 0)
-    return this.board[x][y] ; /// return the number of the winner
-    return 0;
+  const tile1=this.board[x][one];
+
+  if(tile1===this.board[x][two]
+    && tile1===this.board[x][three]
+    && tile1!==0)  return tile1;
+    else return 0;
+
 }
 
  checkHorizontal(x,y){
     //moving the x
-    if(this.board[x][y]==this.board[x+1][y]
-      && this.board[x-1][y]==this.board[x][y] && this.board[x][y]!== 0)
-      return this.board[x][y] ; /// return the number of the winner
-      return 0;
+      const tile1=this.board[one][y];
+    if(tile1===this.board[two][y]
+      && tile1===this.board[three][y]
+      && tile1!==0)  return tile1;
+      else return 0;
 }
 
  checkDiagnolLeft(x,y){
    //moving x,y diagno left
-   if(this.board[x][y]==this.board[x-1][y+1]
-     && this.board[x+1][y-1]==this.board[x][y] && this.board[x][y]!== 0)
-     return this.board[x][y] ; /// return the number of the winner
-     return 0;
+   if(x!==y) return 0;
+
+  const tile1=this.board[one][one];
+ if(tile1===this.board[two][two]
+   && tile1===this.board[three][three]
+   && tile1!==0)  return tile1;
+   else return 0;
 
 }
 
  checkDiagonalRight(x,y){
-   if(this.board[x][y]==this.board[x+1][y+1]
-     && this.board[x-1][y-1]==this.board[x][y] && this.board[x][y]!== 0)
-     return this.board[x][y] ; /// return the number of the winner
-     return 0;
+
+  const tile1=this.board[three][one];
+ if(tile1===this.board[two][two]
+   && tile1===this.board[one][three]
+   && tile1!==0)  return tile1;
+   else return 0;
 }
 
 checkWinner() {
